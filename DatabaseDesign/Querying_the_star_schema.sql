fe40521567ce88acda880d64da5dda1c57049f80
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2024-10-20 12:18:36 +0100

    2_3

---
+++ DatabaseDesign/Querying_the_star_schema.sql
@@ -0,0 +1,15 @@
+-- Output each state and their total sales_amount
+SELECT dim_store_star.state, sum(sales_amount)
+FROM fact_booksales
+	-- Join to get book information
+    JOIN dim_book_star ON fact_booksales.book_id = dim_book_star.book_id
+	-- Join to get store information
+    JOIN dim_store_star ON fact_booksales.store_id = dim_store_star.store_id
+-- Get all books with in the novel genre
+WHERE
+    dim_book_star.genre = 'novel'
+-- Group results by state
+GROUP BY
+    dim_store_star.state;
+
+
